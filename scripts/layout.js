411e0935b6f71e321135ac2994d3005fce58b3b1
Abitta <jakechamorro12@gmail.com>
2026-09-01 03:37:56 -0300

    Integracion de layout y se actualiza contador del carrito

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,18 +1,14 @@
-function loadPartial(elementId, file) {
-
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        })
-        .catch(error => console.error("No se pudo cargar " + file, error));
-
-}
-
-function loadLayout() {
-
-    loadPartial("header", "partials/header.html");
-    loadPartial("footer", "partials/footer.html");
-
+function cargarLayout() {
+    const headerFetch = fetch("partials/header.html")
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById("header").innerHTML = html;
+    });
+    const footerFetch = fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(html => {
+    document.getElementById("footer").innerHTML = html;
+    });
+    return Promise.all([headerFetch, footerFetch]);
 }
 
